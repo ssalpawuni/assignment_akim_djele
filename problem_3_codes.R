a45588057d3db8c9f1c5a33a3aaf61539f84af35
Abubakari Sumaila Salpawuni <60129306+ssalpawuni@users.noreply.github.com>
2021-06-12 18:58:52 +0300

    log returns incorporated

--- problem_3_codes.R
+++ problem_3_codes.R
@@ -8,7 +8,7 @@ library("zoo")
 
 # TASK 1 ------------------------------------------------------------------
 ########################################################
-### TASK 1: Looping 10 stock prices of S&P500
+### TASK 1: Retrieving 10 stock prices of S&P500
 ########################################################
 tickers <- c("AAPL", "MSFT", "AMZN", "GOOGL", "BRK-B",
              "JPM", "JNJ", "NVDA", "BAC", "PFE")
@@ -21,6 +21,7 @@ for(i in tickers){ # retrieve returns from Jan 2010 to Jan 2020
                                       provider = "yahoo", origin = "2000-09-01",
                                       compression = "m", retclass = "zoo")
 }
+
 sapply(stock_prices, length) # checking lengths of downloaded RETURNs (i.e 120)
 
 # TASK 2 ------------------------------------------------------------------
@@ -31,14 +32,17 @@ len = 120 # number of months from Jan 2010 to Jan 2021
 ones <- matrix(data = 1, nrow = len) # matrix of ones
 mat.x <- sapply(stock_prices, unclass) # obtain matrix of 10 securities
 
+# Transformation (log returns of stock prices)
+mat.r <- apply(mat.x, 2, log)
+
 # mean vector
-mu_cap <- 1/len * t(ones) %*% mat.x # mean returns across 10 securities
+mu_cap <- 1/len * t(ones) %*% mat.r # mean returns across 10 securities
 
 # mean matrix
 mu_mat <- matrix(data = mu_cap, ncol = 10, nrow = 120, byrow = TRUE)
 
 # variances and covariances
-var_cov <- 1/(len - 1) * t((mat.x - mu_mat)) %*% (mat.x - mu_mat)
+var_cov <- 1/(len - 1) * t((mat.r - mu_mat)) %*% (mat.r - mu_mat)
 
 # To get the variance elements, perform diag(diag(var_cov))
 # To get the covariance elements, perform var_cov - diag(diag(var_cov))
@@ -52,6 +56,10 @@ var_cov <- 1/(len - 1) * t((mat.x - mu_mat)) %*% (mat.x - mu_mat)
 
 ## Method II (Optimization)
 
+log(get.hist.quote(instrument = "AAPL", start = "2010-01-01",
+                   end = "2020-01-01", quote = "AdjClose",
+                   provider = "yahoo", origin = "2000-09-01",
+                   compression = "m", retclass = "zoo"))
 
 
 
