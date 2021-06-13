0786ed51721d27fe047a0463fb577593b21b3365
Abubakari Sumaila Salpawuni <60129306+ssalpawuni@users.noreply.github.com>
2021-06-13 07:33:17 +0300

    revised log differencing of returns

--- problem_3_codes.R
+++ problem_3_codes.R
@@ -25,26 +25,27 @@ for(i in tickers){ # retrieve returns from Jan 2010 to Jan 2020
 
 sapply(stock_prices, length) # checking lengths of downloaded RETURNs (i.e 120)
 
+# Transformation of stock prices (log differencing)
+log_ret <- lapply(X = stock_prices, FUN = function(x) diff(log(x)))
+
+mat.r <- sapply(log_ret, unclass) # obtain matrix of 10 securities
+
 # TASK 2 ------------------------------------------------------------------
 ########################################################
 ### Computing summary statistics
 ########################################################
-len = 120 # number of months from Jan 2010 to Jan 2021
-ones <- matrix(data = 1, nrow = len) # matrix of ones
-mat.x <- sapply(stock_prices, unclass) # obtain matrix of 10 securities
+N = length(tickers) # number of securities (10)
+len = nrow(mat.r) # numbers of  diff months from Jan 2010 to Jan 2021
+ones <- matrix(data = 1, nrow = N) # matrix of ones
 
-# Transformation (log returns of stock prices)
-mat.r <- apply(mat.x, 2, log)
-# lapply(X = stock_prices, FUN = function(x) diff(log(x))) log differencing
-
-# mean vector --- You may also use crossprod() ---
-mu_cap <- 1/len * t(mat.r) %*% ones # mean returns across 10 securities
+# mean vector --- You may also use crossprod() --- (across 10 securities)
+mu_cap <- 1/len * t(mat.r) %*% matrix(1, nrow = len)
 
 # mean matrix
-mu_mat <- matrix(data = mu_cap, ncol = 10, nrow = 120, byrow = TRUE)
+mu_mat <- matrix(data = mu_cap, ncol = N, nrow = len, byrow = TRUE)
 
 # variances and covariances
-var_cov <- 1/(len - 1) * t((mat.r - mu_mat)) %*% (mat.r - mu_mat)
+vcov <- 1/(len - 1) * t((mat.r - mu_mat)) %*% (mat.r - mu_mat)
 
 # To get the variance elements, perform diag(diag(var_cov))
 # To get the covariance elements, perform var_cov - diag(diag(var_cov))
@@ -57,19 +58,14 @@ var_cov <- 1/(len - 1) * t((mat.r - mu_mat)) %*% (mat.r - mu_mat)
 ## Method I (Lagrangian)
 
 ## Method II (Optimization)
-ons <- matrix(data = 1, nrow = 10, ncol = 1) # matrix of ones with dim = 10x1
-mat_A <- t(mu_cap) %*% var_cov^-1 %*% ons # Matrix A
-mat_B <- t(mu_cap) %*% var_cov^-1 %*% mu_cap # Matrix B
-mat_C <- t(ons) %*% var_cov^-1 %*% ons # matrix C
-mat_D <- mat_B %*% mat_C - mat_A %*% mat_A
+mat_A <- t(mu_cap) %*% vcov^-1 %*% ones # Matrix A
+mat_B <- t(mu_cap) %*% vcov^-1 %*% mu_cap # Matrix B
+mat_C <- t(ones) %*% vcov^-1 %*% ones # matrix C
+mat_D <- mat_B %*% mat_C - mat_A %*% mat_A 
 
 # TO BE CONTINUED ...
 
 # Code below not necessary ------------------------------------------------
-log(get.hist.quote(instrument = "AAPL", start = "2010-01-01",
-                   end = "2020-01-01", quote = "AdjClose",
-                   provider = "yahoo", origin = "2000-09-01",
-                   compression = "m", retclass = "zoo"))
 
 
 
